b00da649da0b3468c9b5b4c31f8fbc7ad69ef0a2
Shivani810 <134269196+Shivani810@users.noreply.github.com>
2023-06-20 12:02:54 +0200

    added logo and effects on navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -6,6 +6,9 @@ export let title = "BonVoyage"
 
 <div class="navbar">
     <a class="title" href="/">{title}</a>
+    <a class="logo" href="/">
+        <img src="/icons8-wedding-travel-48.png" alt="Logo">
+    </a>    
     <a href="/ourhistory">Our History</a>
     <a href="/ourservices">Our Services</a>
     <a href="/subscribe">Subscribe</a>
@@ -28,7 +31,7 @@ export let title = "BonVoyage"
             text-align: center;
             padding: 14px 16px;
             text-decoration: none;
-            font-size: 18px;
+            font-size: 20px;
         }
         
         .navbar a:hover {
@@ -38,8 +41,18 @@ export let title = "BonVoyage"
         .navbar .title{
             float: left;
             padding: 14px 16px;
-            font-size: 18px;
-            font-weight: bold;
+            font-size: 20px;
         }
+
+        .navbar .logo img {
+        float: left;
+        height: 22.5px; 
+        margin-right: 0px; 
+    }
+
+    .navbar .logo:hover {
+        background-color: initial; /* Reset the background color */
+        transform: scale(1.2);
+    }
 
    </style>
